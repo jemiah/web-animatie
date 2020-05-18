951ee8046b97fd0cfd48dcf74ea9c742bccfbf39
jemiah <43675747+jemiah@users.noreply.github.com>
2020-05-18 10:43:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+  element.classList.toggle("kleuren");
+});
+
+var pop = document.getElementById("rood-pop");
+var groen = document.getElementById("groen");
+var blauwlinks = document.getElementById("blauw_links");
+var blauwrechts = document.getElementById("blauw_rechts)");
+
+var roze = "#DB395B";
+var oranje = "#EEB496";
+var zalm =  "#F7E5D2";  
