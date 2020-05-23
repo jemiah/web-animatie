63c09d863b33bbf9eac715d768bc58e99ea2c365
jemiah <43675747+jemiah@users.noreply.github.com>
2020-05-23 18:41:08 +0200

    update main.css

--- js/main.js
+++ js/main.js
@@ -1,14 +1,53 @@
-var button = document.querySelector("button");
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
 
-button.addEventListener("click", function() {
-  element.classList.toggle("kleuren");
-});
 
-var pop = document.getElementById("rood-pop");
-var groen = document.getElementById("groen");
-var blauwlinks = document.getElementById("blauw_links");
-var blauwrechts = document.getElementById("blauw_rechts)");
 
-var roze = "#DB395B";
-var oranje = "#EEB496";
-var zalm =  "#F7E5D2";  
+/*knop die de kleur veranderd*/
+
+/*element selecteren*/
+var pop = document.querySelector(".rood-pop");
+var button = document.querySelector(".knop");
+var button2 = document.querySelector(".knop2");
+var blauwLinks = document.querySelector(".blauw_links");
+var blauwRechts = document.querySelector(".blauw_rechts");
+var groen = document.querySelector(".groen");
+
+/*luisteren naar een event*/
+button.addEventListener('click', toggle);
+
+
+/*css aanpassen, class toevoegen*/
+function toggle() {
+    pop.classList.toggle('kleur1');
+    blauwLinks.classList.toggle('kleur2');
+    blauwRechts.classList.toggle('kleur2');
+    groen.classList.toggle('kleur3');
+    
+    console.log("test3");
+}
+
+/*knop die pop laat verdwijnen*/
+
+/*luisteren naar een event voor spatiebalk en knop*/
+window.addEventListener('keydown',verdwijn);
+button2.addEventListener('click',verdwijn2);
+
+/*css aanpassen, class toevoegen*/
+function verdwijn(event){
+    if (event.keyCode === 32){
+        groen.classList.toggle('groen2');
+        blauwLinks.classList.toggle('bl');
+        blauwRechts.classList.toggle('br');
+        pop.classList.toggle('roodpoppetje');
+    }
+}
+
+function verdwijn2(){
+groen.classList.toggle('groen2');
+blauwLinks.classList.toggle('bl');
+blauwRechts.classList.toggle('br');
+pop.classList.toggle('roodpoppetje');  
+}
+ 
